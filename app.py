96c7166116794afd2b2a549740dc1fc0a54c6226
Janak Mandavgade <janak2703@gmail.com>
2025-07-12 19:33:31 +0530

    Changed Host

--- app.py
+++ app.py
@@ -456,4 +456,4 @@ def upload_video_to_youtube(
 
 if __name__ == "__main__":
     # mcp.run(transport="streamable-http",host="127.0.0.1", port=8000, path="/mcp")
-    mcp.run(transport="streamable-http", port=8000, path="/mcp")
+    mcp.run(transport="streamable-http",host="0.0.0.0", port=8000, path="/mcp")
